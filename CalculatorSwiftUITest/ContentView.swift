0aaec7449b1f8dbd27930b0559c72b3f08641a56
ybilyar <46769350+ybilyar@users.noreply.github.com>
2020-06-26 14:50:02 -0500

    Initial Commit

---
+++ CalculatorSwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorSwiftUITest
+//
+//  Created by Yurii Bilyar on 6/26/20.
+//  Copyright © 2020 Yurii Bilyar/Postevka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
